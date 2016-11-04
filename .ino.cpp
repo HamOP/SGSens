2804d83b744f8bc7185e5b9ab8dfdb23332f4e28
HamOP <stefan@192.168.178.105>
2016-11-04 20:18:42 +0100

    Test from eclipse

---
+++ .ino.cpp
@@ -0,0 +1,20 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-11-04 20:17:05
+
+#include "Arduino.h"
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include <JetiExProtocol.h>
+void setup() ;
+void loop() ;
+double getVoltage() ;
+double getPressure() ;
+void readAlti() ;
+
+#include "SGSens.ino"
+
+
+#endif
